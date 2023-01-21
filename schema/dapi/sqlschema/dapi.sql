2a2c6293a8227f2995d21b5c77dd1d6e12e0b291
Andreas Hubel <andreas.hubel@pub.tech>
2023-01-21 23:48:51 +0100

    manual schema optimizations

--- schema/dapi/sqlschema/dapi.sql
+++ schema/dapi/sqlschema/dapi.sql
@@ -1,87 +1,128 @@
+CREATE TABLE places (
+	id TEXT PRIMARY KEY,
+	url TEXT,
+	name TEXT,
+	address JSONB
+);
+COMMENT ON TABLE events IS '@sameAs https://schema.org/Place';
+
+CREATE TABLE organization_legal_form (
+		type TEXT PRIMARY KEY,
+		title TEXT UNIQUE,
+		description TEXT,
+		id TEXT
+);
+COMMENT ON TABLE organization_legal_form IS '@enum';
+
+/*
+-- INSERT INTO item_type (type, description) VALUES
+		('Episode', 'An episode is a complete video/audio. Episodes can be assigned to seasons or shows.'),
+		('EventLivestream', 'An event livestream is a time limited livestream valid for a certain event, e.g. a concert or sport match.'),
+		('Section', 'A section is a part of an episode, e.g. a scene of an episode.'),
+		('Extra', 'An extra is a special broadcast about another asset and can be used on items or groupings. Extras can be for example trailers, interviews, making-ofs or behind-the-scenes.');
+*/
 
 
-CREATE TABLE "Address" (
-	street TEXT, 
-	city TEXT, 
-	postal_code TEXT, 
-	PRIMARY KEY (street, city, postal_code)
+CREATE TABLE organizations (
+	id TEXT PRIMARY KEY,
+	url TEXT,
+	name TEXT,
+	title TEXT,
+	legal_form TEXT REFERENCES organization_legal_form(type),
+	category TEXT,
+	description TEXT,
+	image JSONB,
+	address TEXT,
+	mission_statement TEXT,
+	founding_date DATE, -- TODO inception? – What about years without day and month?
+	founding_location TEXT REFERENCES places(id),
+	parent text REFERENCES organizations(id) DEFERRABLE INITIALLY DEFERRED
+);
+COMMENT ON TABLE events IS '@sameAs https://schema.org/Organization';
+
+
+CREATE TABLE projects (
+	id TEXT PRIMARY KEY,
+	url TEXT,
+	name TEXT,
+	description TEXT,
+	image JSONB,
+	start_date DATE,
+	end_date DATE,
+	is_current BOOLEAN
 );
 
-CREATE TABLE "Concept" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	image TEXT, 
-	url TEXT, 
+CREATE TABLE products (
+	id TEXT NOT NULL,
+	name TEXT,
+	description TEXT,
+	image JSONB,
+	url TEXT,
+	start_date DATE,
+	end_date DATE,
+	is_current BOOLEAN,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE "Event" (
-	start_date DATE, 
-	end_date DATE, 
-	duration FLOAT, 
-	is_current BOOLEAN, 
-	PRIMARY KEY (start_date, end_date, duration, is_current)
+CREATE TABLE system_domain (
+	type TEXT PRIMARY KEY,
+	title TEXT UNIQUE,
+	description TEXT,
+	id TEXT
+);
+COMMENT ON TABLE system_domain IS '@enum';
+
+
+CREATE TABLE systems (
+	url TEXT PRIMARY KEY,
+	domain TEXT REFERENCES system_domain(type),
+	name TEXT,
+	description TEXT,
+	start_date DATE,
+	end_date DATE,
+	is_current BOOLEAN
 );
 
-CREATE TABLE "NamedThing" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	image TEXT, 
-	url TEXT, 
+
+CREATE TABLE relationship_type (
+	type TEXT PRIMARY KEY,
+	description TEXT,
+	id TEXT
+);
+COMMENT ON TABLE relationship_type IS '@enum';
+
+CREATE TABLE relationships (
+	type TEXT REFERENCES relationship_type(type),
+	from_id TEXT NOT NULL,
+	to_id TEXT NOT NULL,
+	start_date DATE,
+	end_date DATE,
+	related_to TEXT,
+	PRIMARY KEY (from_id, to_id, type)
+);
+
+
+CREATE TABLE concepts (
+	type TEXT,
+	id TEXT PRIMARY KEY,
+	name TEXT,
+	description TEXT,
+	image JSONB,
+	url TEXT,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE "Place" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	PRIMARY KEY (id)
+CREATE TABLE events (
+	type TEXT,
+	id TEXT PRIMARY KEY,
+	start_date TIMESTAMPTZ NOT NULL,
+	end_date TIMESTAMPTZ,
+	duration INTERVAL,
+	is_current BOOLEAN,
+	url TEXT
 );
+COMMENT ON TABLE events IS '@sameAs https://schema.org/Event';
 
-CREATE TABLE "Product" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	image TEXT, 
-	url TEXT, 
-	start_date DATE, 
-	end_date DATE, 
-	is_current BOOLEAN, 
-	PRIMARY KEY (id)
-);
 
-CREATE TABLE "Project" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	image TEXT, 
-	url TEXT, 
-	start_date DATE, 
-	end_date DATE, 
-	is_current BOOLEAN, 
-	PRIMARY KEY (id)
-);
+ALTER TABLE events OWNER TO editor;
 
-CREATE TABLE "Relationship" (
-	start_date DATE, 
-	end_date DATE, 
-	related_to TEXT, 
-	type TEXT, 
-	PRIMARY KEY (start_date, end_date, related_to, type)
-);
-
-CREATE TABLE "Organization" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	image TEXT, 
-	url TEXT, 
-	legal_form TEXT, 
-	address TEXT, 
-	mission_statement TEXT, 
-	founding_date TEXT, 
-	founding_location TEXT, 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(founding_location) REFERENCES "Place" (id)
-);
